e59a3d6f794f982b13d5d0b04b45f1a63c37bdba
Julian Nite Yu <juliannite678@gmail.com>
2023-03-20 01:56:55 +0800

    Add PORT to env.example

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 const express = require("express")
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.use(express.static(__dirname + "/src"));
 app.get("/", (req, res) => {
